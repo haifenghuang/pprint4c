58e55624f49ea958ceecbfb6708f6f80fd27ae17
hhuang22 <haifeng.huang@dxc.com>
2019-09-17 18:04:50 +0800

    Modify mtest.c

--- mtest.c
+++ mtest.c
@@ -1,38 +1,7 @@
 #include <stdio.h> //printf
-#include <stdlib.h> //calloc, system
-#include <sys/types.h>
-#include <unistd.h>
+#include <string.h> //memset
 
-#define STRINGIZE(A) #A
-
-extern const char *__progname;
-
-static void printout_struct(char *instanceName, void* invar, char* structname) {
-    char dbx[1024];
-    char gdbcmds[64];
-    char structdump[64];
-
-    pid_t pid = getpid();
-
-    sprintf(gdbcmds, "/tmp/gdbcmds.%d", pid);
-    sprintf(structdump, "/tmp/struct.dump.%d", pid);
-
-    sprintf(dbx, "echo 'set print pretty on\np (struct %s)*%p\n' > %s", structname, invar, gdbcmds);
-    system(dbx);
-
-    sprintf(dbx, "echo 'where\ndetach' | gdb -batch --command=%s %s %d > %s", 
-            gdbcmds, __progname, pid, structdump);
-    system(dbx);
-
-    /* remove the first line and replace '$1' with instanceName, then cat the file */
-    sprintf(dbx, "sed -i -e 's/\\$1/%s/' -e '1d' %s; cat %s", instanceName, structdump, structdump);
-    system(dbx);
-
-    unlink(gdbcmds);
-    unlink(structdump);
-
-    return;
-}
+#include "pprint.h"
 
 struct person
 {
@@ -44,23 +13,22 @@ struct person
     }like;
 };
 
-static struct person *johndoe;
-
 int main()
 {
 
-    johndoe = (struct person *)calloc(1, sizeof(struct person));
+    struct person johndoe;
 
-    johndoe->age = 6; 
-    johndoe->like.type = "Software-Developing"; 
-    johndoe->like.name = "C"; 
-    printout_struct(STRINGIZE(johndoe), johndoe, "person"); 
+    memset(&johndoe, 0x00, sizeof(struct person));
 
-    johndoe->age = 8; 
-    printout_struct(STRINGIZE(johndoe), johndoe, "person"); 
+    johndoe.age = 6; 
+    johndoe.like.type = "Software-Developing"; 
+    johndoe.like.name = "C"; 
+    printout_struct(STRINGIZE(johndoe), &johndoe, "person"); 
 
-    printf("Hello World - age: %d\n:", johndoe->age);
+    johndoe.age = 8; 
+    printout_struct(STRINGIZE(johndoe), &johndoe, "person"); 
 
-    free(johndoe);
+    printf("Hello World - age: %d\n:", johndoe.age);
+
     return 0;
 }
